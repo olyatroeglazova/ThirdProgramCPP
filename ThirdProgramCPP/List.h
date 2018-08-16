fc782a81bea4144bb25aca55eb560f26ec64f853
Olga Troeglazova <olya.troeglazova@mail.ru>
2018-08-17 00:05:00 +0600

    .

--- ThirdProgramCPP/List.h
+++ ThirdProgramCPP/List.h
@@ -6,7 +6,7 @@ class List {
 public:
 	virtual void push(Iterator<T>*,T ) = 0;
 	virtual void remove(Iterator<T>*) = 0;
-	virtual Iterator<T> *findFirst(T elem)  = 0;
+	virtual Iterator<T> *findFirst(T )  = 0;
 	virtual void doEmpty() = 0;
 	virtual bool isEmpty() const = 0;
 	virtual int length() const = 0;
